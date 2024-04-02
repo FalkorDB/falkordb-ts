2da844086d2f0bef91b5809ebc8fb080a5b7d328
Guy Korland <gkorland@gmail.com>
2024-04-02 11:01:53 +0300

    add command SENTINEL_MASTERS

---
+++ src/commands/SENTINEL_MASTERS.ts
@@ -0,0 +1,7 @@
+export const IS_READ_ONLY = true;
+
+export function transformArguments(): Array<string> {
+        return ['SENTINEL', 'MASTERS'];
+}
+
+export declare function transformReply(): Array<Array<string>>;
