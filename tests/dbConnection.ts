7610e0aaafd043eec56e863e64034dd1b2f693db
Naseem Ali <34807727+Naseem77@users.noreply.github.com>
2024-09-29 19:03:03 +0300

    error handling to the database connection

--- tests/dbConnection.ts
+++ tests/dbConnection.ts
@@ -1,10 +1,15 @@
 import { FalkorDB } from 'falkordb';
 
 export const client = async () => {
-    return await FalkorDB.connect({
-        socket: {
-            host: 'localhost',
-            port: 6379
-        }
-    });
+    try{
+        return await FalkorDB.connect({
+            socket: {
+                host: 'localhost',
+                port: 6379
+            }
+        });
+    } catch (error){
+        console.log('Failed to connect to FalkorDB:', error);
+        throw error;
+    }
 };
