be7f3a9c89b4f30f14448d3b751173a542f52f72
Naseem Ali <34807727+Naseem77@users.noreply.github.com>
2025-06-30 17:12:26 +0300

    Revert accidental change in index, info specs

--- src/commands/INFO.spec.ts
+++ src/commands/INFO.spec.ts
@@ -1,59 +1,25 @@
 import { strict as assert } from 'assert';
-import { transformArguments, IS_READ_ONLY } from './INFO';
+import testUtils, { GLOBAL } from '../test-utils';
+import { transformArguments } from './INFO';
 
 describe('INFO', () => {
-    describe('transformArguments', () => {
-        it('without section parameter', () => {
-            assert.deepEqual(
-                transformArguments(),
-                ['GRAPH.INFO']
-            );
-        });
-
-        it('with section parameter', () => {
-            assert.deepEqual(
-                transformArguments('STATISTICS'),
-                ['GRAPH.INFO', 'STATISTICS']
-            );
-        });
-
-        it('with different section values', () => {
-            // Test various section types
-            assert.deepEqual(
-                transformArguments('MEMORY'),
-                ['GRAPH.INFO', 'MEMORY']
-            );
-
-            assert.deepEqual(
-                transformArguments('CPU'),
-                ['GRAPH.INFO', 'CPU']
-            );
-
-            assert.deepEqual(
-                transformArguments('REPLICATION'),
-                ['GRAPH.INFO', 'REPLICATION']
-            );
-        });
-
-        it('with empty string section', () => {
-            // Empty string should not add the section parameter
-            assert.deepEqual(
-                transformArguments(''),
-                ['GRAPH.INFO']
-            );
-        });
-
-        it('with undefined section', () => {
-            assert.deepEqual(
-                transformArguments(undefined),
-                ['GRAPH.INFO']
-            );
-        });
+    it('transformArguments', () => {
+        assert.deepEqual(
+            transformArguments(),
+            ['GRAPH.INFO']
+        );
     });
 
-    describe('constants', () => {
-        it('IS_READ_ONLY should be true', () => {
-            assert.strictEqual(IS_READ_ONLY, true);
-        });
-    });
+    testUtils.testWithClient('client.graph.info', async client => {
+        assert.deepEqual(
+            await client.graph.info(),
+            [
+                "# Running queries",
+                [],
+                "# Waiting queries",
+                [],
+
+            ]
+        );
+    }, GLOBAL.SERVERS.OPEN);
 });
