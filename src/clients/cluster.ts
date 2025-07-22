f54e4ea0c984d6524ced997c5543bc91d6fc4cea
Anchel135 <yakoveidelman1@gmail.com>
2025-07-22 10:17:07 +0300

    fix ai comments

--- src/clients/cluster.ts
+++ src/clients/cluster.ts
@@ -85,13 +85,21 @@ export class Cluster implements Client {
   }
 
   async list(): Promise<Array<string>> {
-    return (
-      await Promise.all(
-        this.#client.masters.map(async (master) => {
-          return (await this.#client.nodeClient(master)).falkordb.list();
-        })
-      )
-    ).flat();
+    const reply = await Promise.all(
+      this.#client.masters.map(async (master) => {
+        return (await this.#client.nodeClient(master)).falkordb.list();
+      })
+    );
+    const [result, errors] = [
+      reply.filter((r) => !(r instanceof Error)).flat(),
+      reply.filter((r) => r instanceof Error),
+    ];
+
+    if (errors.length > 0) {
+      console.error("Some nodes failed to respond:", errors);
+    }
+
+    return result;
   }
 
   async configGet(configKey: string) {
