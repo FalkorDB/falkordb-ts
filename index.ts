1feec380971a0635ce4ff2daa22a6e27d19813dc
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2025-11-26 19:52:28 +0200

    fix(index): added correct re-export of enums

--- index.ts
+++ index.ts
@@ -1,8 +1,4 @@
+export { ConstraintType, EntityType } from './src/enums';
+
 export { default as Graph } from './src/graph';
-export { ConstraintType, EntityType } from './src/commands/CONSTRAINT_CREATE';
 export { default as FalkorDB } from './src/falkordb';
-
-
-
-
-
