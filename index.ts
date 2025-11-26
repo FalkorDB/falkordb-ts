c9239c8f99c9262acfb4d77c4aa658b160182d7b
Kristiqn Tachev <kristiqn.tachev@gmail.com>
2025-11-26 10:59:54 +0200

    Update FalkorDB index to return also DB Options
    Re-exported from the default export FalkorDBOptions since we want to have extendability of the client itself.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export { default as Graph } from './src/graph';
 export { ConstraintType, EntityType } from './src/graph';
-export { default as FalkorDB } from './src/falkordb';
+export { default as FalkorDB, FalkorDBOptions } from './src/falkordb';
+export { FalkorDBOptions } from './src/falkordb';
 
 
 
