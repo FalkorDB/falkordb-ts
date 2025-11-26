d40aafae4464168fde74e01870512cdcb9b9a3d5
Kristiqn Tachev <kristiqn.tachev@gmail.com>
2025-11-26 11:02:04 +0200

    Remove duplicate export of FalkorDBOptions

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 export { default as Graph } from './src/graph';
 export { ConstraintType, EntityType } from './src/graph';
 export { default as FalkorDB, FalkorDBOptions } from './src/falkordb';
-export { FalkorDBOptions } from './src/falkordb';
 
 
 
